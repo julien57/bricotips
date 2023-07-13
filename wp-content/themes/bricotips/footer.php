25af442329e18c77ac34b163e411b2454213e959
Julien Montel <julienmontel@MacBook-Pro-de-Julien.local>
2023-07-13 12:02:41 +0200

    step 6 - Creating Widget Image / Titre

---
+++ wp-content/themes/bricotips/footer.php
@@ -0,0 +1,7 @@
+        <footer id="site-footer" class="header-footer-group">
+            @BricoTips depuis 2023
+        </footer>
+
+        <?php wp_footer(); ?>
+    </body>
+</html>
--- wp-content/themes/bricotips/functions.php
+++ wp-content/themes/bricotips/functions.php
@@ -3,4 +3,14 @@
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles() {
 	wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+	wp_enqueue_style('theme-style', get_stylesheet_directory_uri() . '/css/theme.css', [], filemtime(get_stylesheet_directory() . '/css/theme.css'));
+	wp_enqueue_style('image-titre-widget', get_stylesheet_directory_uri() . '/css/widgets/image-titre-widget.css', [], filemtime(get_stylesheet_directory() . '/css/widgets/image-titre-widget.css'));
 }
+
+// Chargement des widgets
+require_once(__DIR__ . '/widgets/ImageTitreWidget.php');
+
+function register_widgets() {
+	register_widget('Image_Titre_Widget');
+}
+add_action('widgets_init', 'register_widgets');
